cb634412983d4d787455573efd65eddf13b6b17d
JSchoppe <jethro.schoppenhorst@gmail.com>
2020-02-21 21:12:43 -0600

    Removed regex dependency
    Removed regex as it is unsupported by the compiler

--- Assignment1_Cafe/Cafe.cpp
+++ Assignment1_Cafe/Cafe.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <regex>
+#include <vector>
 #include "MenuItem.cpp"
+
+std::string StringToLower(std::string);
+std::vector<std::string> StringSplit(std::string, char);
 
 int main()
 {
     // Define recognized input patterns.
-    const std::regex helpCommand("^((H|h)elp|HELP)$");
-    const std::regex listCommand("^((L|l)ist|LIST)$");
-    const std::regex exitCommand("^((E|e)xit|EXIT)$");
-    const std::regex buyCommand("^([A-Za-z]+\\s[0-9]+(\\s)*)+$");
+    const std::string helpCommand = "help";
+    const std::string listCommand = "list";
+    const std::string exitCommand = "exit";
     // Define the available menu items.
     const MenuItem items[4] = {
-        MenuItem("Coffee", 2.15f),
-        MenuItem("Tea", 2.75f),
-        MenuItem("Sandwich", 3.50f),
-        MenuItem("Burger", 5.25f)
+        MenuItem("coffee", 2.15f),
+        MenuItem("tea", 2.75f),
+        MenuItem("sandwich", 3.50f),
+        MenuItem("burger", 5.25f)
     };
+    const size_t length = 4;
 
     // Primary program loop.
     bool inProgram = true;
@@ -27,9 +30,10 @@ int main()
         std::cout << "Enter a command. Use 'help' to see commands." << std::endl;
         std::string userInput;
         std::getline(std::cin, userInput);
+        userInput = StringToLower(userInput);
 
         // Check user input against commands.
-        if (std::regex_match(userInput, helpCommand))
+        if (userInput == helpCommand)
         {
             // Show tips for the available commands.
             std::cout << "Available commands:" << std::endl;
@@ -37,9 +41,8 @@ int main()
             std::cout << "{MenuItem} {Quantity}: calculates total cost for an order" << std::endl;
             std::cout << "exit: ends the program" << std::endl;
         }
-        else if (std::regex_match(userInput, listCommand))
+        else if (userInput == listCommand)
         {
-            const size_t length = sizeof(items) / sizeof(items[0]);
             for (int i = 0; i < length; i++)
             {
                 // Print each shop item.
@@ -47,64 +50,90 @@ int main()
                 std::cout << " " << items[i].name << std::endl;
             }
         }
-        else if (std::regex_match(userInput, buyCommand))
-        {
-            float totalPrice = 0;
-
-            // Define patterns for matching command arguments.
-            const std::regex itemQuantityExp("[A-Za-z]+\\s[0-9]+");
-            const std::regex itemExp("[A-Za-z]+");
-            const std::regex quantityExp("[0-9]+");
-
-            // Match all item quantity pairs.
-            std::smatch itemQuantityMatch;
-            do
-            {
-                // Advance to the next expression pair.
-                std::regex_search(userInput, itemQuantityMatch, itemQuantityExp);
-                std::string itemQuantityPair = itemQuantityMatch[0];
-
-                // Extract the components of this pair.
-                std::smatch itemMatch, quantityMatch;
-                std::regex_search(itemQuantityPair, itemMatch, itemExp);
-                std::regex_search(itemQuantityPair, quantityMatch, quantityExp);
-                std::string item = itemMatch[0];
-                int quantity = std::stoi(quantityMatch[0]);
-
-                if (quantity > 0)
-                {
-                    // Search for this shop item.
-                    const size_t length = sizeof(items) / sizeof(items[0]);
-                    for (int i = 0; i < length; i++)
-                    {
-                        if (items[i].name == item)
-                        {
-                            // Print receipt line.
-                            std::cout << "$" << std::fixed << std::setprecision(2) << items[i].price;
-                            std::cout << " X " << quantity << " " << item << std::endl;
-                            totalPrice += items[i].price * quantity;
-                            break;
-                        }
-                    }
-                }
-
-                // Move past this expression pair.
-                userInput = itemQuantityMatch.suffix();
-            } while (userInput != "");
-
-            // If any item matches were found print the total.
-            if (totalPrice > 0)
-            {
-                std::cout << "TOTAL: $" << std::fixed << std::setprecision(2) << totalPrice << std::endl;
-            }
-        }
-        else if (std::regex_match(userInput, exitCommand))
+        else if (userInput == exitCommand)
         {
             // Exit program loop.
             inProgram = false;
+        }
+        else
+        {
+            // Split the input into chunks.
+            std::vector<std::string> inputChunks = StringSplit(userInput, ' ');
+
+            // Are there at least two arguments?
+            if(inputChunks.size() >= 2)
+            {
+                // Initialize arguments to invalid values.
+                size_t selectedItemIndex = -1;
+                int selectedItemQuantity = -1;
+
+                // Search for the selected menu item.
+                for (int i = 0; i < length; i++)
+                {
+                    if(inputChunks[0] == items[i].name)
+                    {
+                        selectedItemIndex = i;
+                        break;
+                    }
+                }
+
+                // Try to parse a quantity.
+                try
+                {
+                    selectedItemQuantity = std::stoi(inputChunks[1]);
+                }
+                catch(...) { }
+
+                // If there was valid input, print the price.
+                if(selectedItemIndex != -1 && selectedItemQuantity > 0)
+                {
+                    // Print each shop item.
+                    std::cout << "$" << std::fixed << std::setprecision(2)
+                        << items[selectedItemIndex].price * selectedItemQuantity;
+                }
+            }
         }
 
         // Add spacing between commands.
         std::cout << std::endl;
     }
+}
+
+std::vector<std::string> StringSplit(const std::string toSplit, const char delimiter)
+{
+    std::vector<std::string> returnVector;
+    int length = toSplit.length();
+    std::string currentChunk = "";
+    for(int i = 0; i < length; i++)
+    {
+        // Concatenate characters until a delimiter is hit.
+        if(toSplit[i] != delimiter)
+        {
+            currentChunk += toSplit[i];
+        }
+        else if(currentChunk != "")
+        {
+            // If the concatenated string has length, push it onto the vector.
+            returnVector.push_back(currentChunk);
+            currentChunk = "";
+        }
+    }
+    // Check the final check at the end of the string.
+    if(currentChunk != "")
+    {
+        returnVector.push_back(currentChunk);
+    }
+    return returnVector;
+}
+
+std::string StringToLower(const std::string string)
+{
+    // Convert each character in the string to lowercase.
+    std::string returnString = "";
+    int length = string.length();
+    for(int i = 0; i < length; i++)
+    {
+        returnString += std::tolower(string[i]);
+    }
+    return returnString;
 }
